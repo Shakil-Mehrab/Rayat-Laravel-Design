7ad9403c77494150d3a0c21e66d364ed8c602f8a
Shakil-Mehrab <mehrabhoussainshakil12@gmail.com>
2021-06-12 20:46:41 +0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="top header">
+<div class="container">
+    <div class="row">
+    <div class="cal-md-6">
+    <div class="cal-md-4">
+    
+    </div>
+    <div class="cal-md-8">
+    <span>Product</span>
+    </div>
+    <div class="cal-md-6">
+    
+    </div>
+    </div>
+    </div>
+</div>
+
+</body>
+</html>
