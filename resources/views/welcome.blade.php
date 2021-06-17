6e1eab4e1dd0d03d44c27341c600018f04bc61e1
Kuyasha-Afrim <kuyashaafrim18@gmail.com>
2021-06-17 18:52:29 +0600

    2nd commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<div class="top header">
-<div class="container">
-    <div class="row">
-    <div class="cal-md-6">
-    <div class="cal-md-4">
-    
-    </div>
-    <div class="cal-md-8">
-    <span>Product</span>
-    </div>
-    <div class="cal-md-6">
-    
-    </div>
-    </div>
-    </div>
-</div>
 
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://kit.fontawesome.com/bb2f33706c.js" crossorigin="anonymous"></script>
+  <title>Document</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <div class="top-header">
+    <div class="col-md-6">
+      <div class="row">
+        <strong>Alibaba.com</strong>
+        <h4>Product</h4>
+      </div>
+    </div>
+    <div class="col-md-6">
+    <div class="low-header navbar-fiext-top" style="background:black;padding: 10px;">
+        <ul style="display: flex;justify-content: space-between;">
+
+            <li class="nav_li"><a href="#">
+                    <span><i class="fas fa-user"></i></span> Sign up
+                </a></li>
+            <li class="nav_li"><a href="#">
+                    <span><i class="fab fa-page4"></i></span> Message
+                </a></li>
+            <li class="nav_li"><a href="#"><span><i class="fab fa-product-hunt"></i></span>
+                    Order
+                </a></li>
+            <li class="nav_li"><a href="#"><span><i class="fas fa-blog"></i></span>
+                    Cart
+                </a></li>
+        </ul>
+    </div>
+    </div>
+
+  </div>
 </body>
+
 </html>
--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-})
-// Product
-Route::get('/product',[App\Http\Controllers\Product\ProductController::class, '']);
-
+});
 
